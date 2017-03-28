16677cd2505c51dbfe6bd488ebdd6f12a52f0554
ckelner <chris.kelner@gmail.com>
2017-03-27 22:26:12 -0400

    Move all rendering to server side

--- app/src/client/index.jsx
+++ app/src/client/index.jsx
@@ -9,16 +9,20 @@ import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { BrowserRouter } from 'react-router-dom'
 import thunkMiddleware from 'redux-thunk'
-
-import App from './app'
-import helloReducer from './reducer/hello'
+import Immutable from 'immutable'
+import App from '../shared/app'
+import helloReducer from '../shared/reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 
-// eslint-disable-next-line no-underscore-dangle
+/* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+const preloadedState = window.__PRELOADED_STATE__
+/* eslint-enable no-underscore-dangle */
 
-const store = createStore(combineReducers({ hello: helloReducer }),
+const store = createStore(combineReducers(
+  { hello: helloReducer }),
+  { hello: Immutable.fromJS(preloadedState.hello) },
   composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
@@ -36,9 +40,9 @@ ReactDOM.render(wrapApp(App, store), rootEl)
 
 if (module.hot) {
   // flow-disable-next-line
-  module.hot.accept('./app', () => {
+  module.hot.accept('../shared/app', () => {
     // eslint-disable-next-line global-require
-    const NextApp = require('./app').default
+    const NextApp = require('../shared/app').default
     ReactDOM.render(wrapApp(NextApp, store), rootEl)
   })
 }
