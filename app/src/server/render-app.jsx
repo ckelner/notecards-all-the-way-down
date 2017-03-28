7f21ebe1a9d10242afe93b43fff895310c944cac
ckelner <chris.kelner@gmail.com>
2017-03-27 22:31:16 -0400

    Update pages with Helmet

--- app/src/server/render-app.jsx
+++ app/src/server/render-app.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import { Provider } from 'react-redux'
 import { StaticRouter } from 'react-router'
-
+import Helmet from 'react-helmet'
 import initStore from './init-store'
 import App from './../shared/app'
 import { APP_CONTAINER_CLASS, STATIC_PATH, WDS_PORT } from '../shared/config'
@@ -18,12 +18,14 @@ const renderApp = (location: string, plainPartialState: ?Object, routerContext: 
         <App />
       </StaticRouter>
     </Provider>)
+  const head = Helmet.rewind()
 
   return (
     `<!doctype html>
     <html>
       <head>
-        <title>FIX ME</title>
+        ${head.title}
+        ${head.meta}
         <link rel="stylesheet" href="${STATIC_PATH}/css/style.css">
       </head>
       <body>
