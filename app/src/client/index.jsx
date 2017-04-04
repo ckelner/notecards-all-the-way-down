cc95daddf13347d0c6cba2d773d4d9b677730f38
ckelner <chris.kelner@gmail.com>
2017-04-04 19:15:57 -0400

    demo "chatroom" for socket.io

--- app/src/client/index.jsx
+++ app/src/client/index.jsx
@@ -14,6 +14,7 @@ import App from '../shared/app'
 import helloReducer from '../shared/reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
+import setUpSocket from './socket'
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
@@ -46,3 +47,5 @@ if (module.hot) {
     ReactDOM.render(wrapApp(NextApp, store), rootEl)
   })
 }
+
+setUpSocket(store)
